908d1b524a1c3ab17c64cd60223bdb9d0e08cd4f
Arthur <arthurleonel7@gmail.com>
2023-03-07 12:04:04 -0300

    Implementing defensive programming

--- boardgame/Board.java
+++ boardgame/Board.java
@@ -7,6 +7,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+        if (rows < 1 || columns < 1) {
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -21,16 +24,39 @@ public class Board {
     }
 
     public Piece piece(int row, int colums) {
+        if (!positionExists(row, colums)) {
+            throw new BoardException("Position off limits");
+        }
         return pieces[row][colums];
     }
 
     public Piece piece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position off limits");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position)) {
+            throw new BoardException("There is already a piece in position " + position);
+        }
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
     }
 
+    private boolean positionExists(int row, int column) {
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position off limits");
+        }
+        return piece(position) != null;
+    }
 }
