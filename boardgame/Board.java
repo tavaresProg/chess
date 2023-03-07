a862f79cbb1e60a4389aa58110692b0ddd1ced4a
Arthur <arthurleonel7@gmail.com>
2023-03-06 21:00:33 -0300

    Created Board and Piece class

---
+++ boardgame/Board.java
@@ -0,0 +1,22 @@
+package boardgame;
+
+public class Board {
+
+    private int rows;
+    private int colums;
+    private Piece[][] pieces;
+
+    public Board(int rows, int colums) {
+        this.rows = rows;
+        this.colums = colums;
+        pieces = new Piece[rows][colums];
+    }
+
+    public int getRows() {
+        return rows;
+    }
+
+    public int getColums() {
+        return colums;
+    }
+}
