7e2420d868a37bc9888ffdb1f595dd6fa093630c
Arthur <arthurleonel7@gmail.com>
2023-03-08 07:48:57 -0300

    added movement

--- application/Main.java
+++ application/Main.java
@@ -1,12 +1,28 @@
 package application;
 
 import chess.ChessMatch;
+import chess.ChessPiece;
+import chess.pieces.ChessPosition;
+
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        ChessMatch cm = new ChessMatch();
-        UI.printBoard(cm.getPieces());
+        Scanner sc = new Scanner(System.in);
+        ChessMatch chessMatch = new ChessMatch();
+
+        while (true) {
+            UI.printBoard(chessMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            ChessPosition source = UI.readChessPosition(sc);
+            System.out.print("Target: ");
+            ChessPosition target = UI.readChessPosition(sc);
+
+            ChessPiece capturedPiece = chessMatch.performeChessMove(source, target);
+
+        }
     }
 }
--- application/UI.java
+++ application/UI.java
@@ -2,6 +2,10 @@ package application;
 
 import chess.ChessPiece;
 import chess.Color;
+import chess.pieces.ChessPosition;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
 
@@ -9,6 +13,17 @@ public class UI {
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_WHITE = "\u001B[37m";
 
+
+    public static ChessPosition readChessPosition(Scanner sc) {
+        try {
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new ChessPosition(column, row);
+        } catch (RuntimeException e) {
+            throw new InputMismatchException("Invalid ChessPosition. Valid values are from a1 to h8.");
+        }
+    }
 
     public static void printBoard(ChessPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
